f09f8b9582360a30d3eac2b36a2e96ddef1963e4
teghfo <teghfo@gmail.com>
2022-08-12 19:17:21 +0430

    es module concept

--- basic/Week2/session2/index.js
+++ basic/Week2/session2/index.js
@@ -1,8 +1,7 @@
-const getUserDataModule = require("./lib/getUserData")
+import getData, {name} from "./lib/getUserData.js"
 
 // getDataModule.getData()
 // console.log(getUserDataModule.name);
 
-setTimeout(() => { console.log("salam"); }, 50000)
-
-console.log("salam birun");
+// setTimeout(() => { console.log("salam"); }, 50000)
+console.log("salam birun", name);
