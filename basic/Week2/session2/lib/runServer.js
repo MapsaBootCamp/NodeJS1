168cddbcc8275debf8b80c9eb13088b8e2377eb3
teghfo <teghfo@gmail.com>
2022-08-12 19:17:53 +0430

    log data in http request

--- basic/Week2/session2/lib/runServer.js
+++ basic/Week2/session2/lib/runServer.js
@@ -1,11 +1,20 @@
-const http = require('http');
-
+import * as http from "http"
 const port = 8000
 
-http.createServer((req, res) => {
-    console.log(req);
+http.createServer(async (req, res) => {
+
+    const buffers = [];
+
+    for await (const chunk of req) {
+      buffers.push(chunk);
+    }
+  
+    const data = Buffer.concat(buffers).toString();
+    console.log(data);
+    // console.log(JSON.parse(data).todo); // 'Buy the milk'
+    
     res.writeHead(200, {"Content-Type": "text/plain"})
-    res.end("Salam")
+    res.end("Salam chetori????")
 
 }).listen(port, "localhost")
 
