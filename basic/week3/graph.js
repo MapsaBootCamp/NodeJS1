ca282e51e56ba4e276a86d042b1a1c1b45366530
teghfo <teghfo@gmail.com>
2022-08-18 19:57:37 +0430

    class example

--- basic/week3/graph.js
+++ basic/week3/graph.js
@@ -1,29 +1,48 @@
 class Graph{
-    #adjMatrix;
 
     constructor(){
-        this.#adjMatrix = {}
+        this._adjMatrix = {}
     }
 
     addNode(nodeName){
-        if(nodeName in Object.keys(this.#adjMatrix))
+        if(nodeName in Object.keys(this._adjMatrix))
             return null;
-        this.#adjMatrix[nodeName] = []
+        this._adjMatrix[nodeName] = []
     }
 
     addEdge(nodeName1, nodeName2){
         this.addNode(nodeName1)
         this.addNode(nodeName2)
-        this.#adjMatrix[nodeName1].push(nodeName2)
-        this.#adjMatrix[nodeName2].push(nodeName1)
+        this._adjMatrix[nodeName1].push(nodeName2)
+        this._adjMatrix[nodeName2].push(nodeName1)
+    }
+    findNeghbors(nodeName, neghborsArray=[]){
+        for(let elm of this._adjMatrix[nodeName]){
+            if(!neghborsArray.includes(elm)){
+                neghborsArray.push(elm);
+                neghborsArray.concat(this.findNeghbors(elm, neghborsArray))
+            }
+        }
+        return neghborsArray;
     }
 
     isConnected(){
-        
+        if(Object.keys(this._adjMatrix).length < 2)
+            return true;
+        const result = this.findNeghbors(Object.keys(this._adjMatrix)[0])
+        console.log(result);
+        if(result.length === Object.keys(this._adjMatrix).length)
+            return true;
+        return false
     }
 
+    findPath(nodeName1, nodeName2){
+
+    }
+
+
     display(){
-        console.log(this.#adjMatrix);
+        console.log(this._adjMatrix);
     }
 }
 
@@ -35,12 +54,17 @@ g1.addNode(3)
 g1.addNode(4)
 g1.addNode(5)
 g1.addNode(6)
+g1.addNode(6)
+g1.addNode(7)
 
 g1.addEdge(1, 2)
+g1.addEdge(1, 3)
+g1.addEdge(3, 5)
 g1.addEdge(1, 6)
 g1.addEdge(2, 4)
+g1.addEdge(5, 7)
 
+console.log(g1.isConnected());
 
-g1.display()
-
+// g1.display()
 
