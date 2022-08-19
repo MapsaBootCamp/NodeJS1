858e1255828fccd427cb65ddb6cda3c9c30f6e9b
teghfo <teghfo@gmail.com>
2022-08-19 20:06:29 +0430

    minor fix

--- basic/week3/graph.js
+++ basic/week3/graph.js
@@ -36,11 +36,33 @@ class Graph{
             return true;
         return false
     }
-
-    findPath(nodeName1, nodeName2){
-
+    
+    _checkExistUnvisitedNeghbor(nodeName, visitedNode){
+        return this._adjMatrix[nodeName].filter((elm) => !visitedNode.includes(elm))
     }
 
+    findPath(nodeName1, nodeName2, path=[nodeName1], visitedNode=[]){
+        visitedNode.push(nodeName1)
+        for(let elm of this._adjMatrix[nodeName1]){
+            if(elm == nodeName2){
+                path.push(elm)
+                return path
+            }else if(visitedNode.includes(elm)){
+                continue;
+            }
+            else if(!this._checkExistUnvisitedNeghbor(elm, visitedNode)){
+                return null;
+            }
+
+            if(!visitedNode.includes(elm)){
+                path.push(elm)
+                if(this.findPath(elm, nodeName2, path, visitedNode) === null){
+                    path.pop()
+                }
+            }
+        }
+        return path;
+    }
 
     display(){
         console.log(this._adjMatrix);
@@ -74,8 +96,10 @@ g1.addEdge(1, 6)
 g1.addEdge(2, 4)
 g1.addEdge(5, 7)
 
-const g2 = new DirectedGraph()
-console.log(g2.jahat());
+// const g2 = new DirectedGraph()
+// console.log(g2.jahat());
+
+console.log(g1.findPath(1, 4));
 
 // console.log(g1.isConnected());
 // console.log(g1.jahat)
