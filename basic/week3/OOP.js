73c5b2c3deec45c15094cc25c3b6bc94bb724711
teghfo <teghfo@gmail.com>
2022-08-19 18:16:06 +0430

    Mixin

--- basic/week3/OOP.js
+++ basic/week3/OOP.js
@@ -66,4 +66,96 @@ class Squire extends Rectangle {
 const recObj = new Rectangle(12, 18)
 const sqrObj = new Squire(12, 12)
 
-console.log(sqrObj.dispaly());
+// console.log(sqrObj.dispaly());
+
+
+//////////////////// mixin with assign
+class Test {
+    constructor() {
+      this.var1 = 'var1'
+    }
+    method1() {
+      console.log(this.var1)
+    }
+    test() {
+      console.log(this.name);
+      this.method2()
+    }
+}
+
+let name = "AS"
+
+var mixin = {
+    var2: 'var2',
+    method2: function(){
+      console.log(this.var2)
+    }
+}
+
+
+Object.assign(Test.prototype, mixin)
+// const t1 = new Test();
+// t1.test()
+
+
+//////////////////// mixin with assign
+let ClassA = (superclass) => class extends superclass{
+    foo(){
+        console.log("sallllllam az foo!");
+    }
+}
+
+class BaseClass{
+    bar(){
+        console.log("sallllllam az bar!");
+    }
+}
+
+class classB extends ClassA(Object){
+
+}
+const obj1 = new classB()
+// obj2.foo()
+obj1.foo()
+
+
+let TestMixiend = (superclass) => class extends superclass{
+    
+    constructor(){
+        super();
+        this.var2 = 'var2'
+    }
+
+    method2(){
+      console.log(this.var2)
+    }
+}
+
+class TestWithMyMixin extends TestMixiend(Test){
+    
+}
+
+const obj = new TestWithMyMixin();
+obj.method2()
+
+
+class A{
+    salam(){
+        console.log("Salam A")
+    }
+}
+
+class B extends A{
+    salam(){
+        super.salam()
+        console.log("Salam B")
+    }
+}
+
+class C extends B{
+    salam(){
+        super.salam()
+    }
+}
+
+const objC = new C()
