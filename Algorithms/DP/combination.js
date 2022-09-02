e9bcb3a83b6e8d29b2c96fb82fe59c91001cbafe
teghfo <teghfo@gmail.com>
2022-09-02 18:59:12 +0430

    dp for binomial coef

--- Algorithms/DP/combination.js
+++ Algorithms/DP/combination.js
@@ -1,8 +1,12 @@
-function comb(n , k) {
-    if(n === k) return 1;
+function comb(n , k, dp=null) {
+    if(dp === null)
+        dp = Array(n + 1).fill(0).map(() => Array(k + 1).fill(-1))
+    if(n === k || k === 0) return 1;
+    else if(n === 0) return 0; 
     else if(k === 1) return n;
-    // else if()
-    return comb(n-1, k) + comb(n-1, k-1);
+    else if(dp[n][k] >= 0) return dp[n][k];
+    dp[n][k] = comb(n-1, k, dp) + comb(n-1, k-1, dp);
+    return dp[n][k];
 }
 
-console.log(comb(50, 22));
+console.log(comb(500, 22));
