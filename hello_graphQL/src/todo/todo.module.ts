da086c0f7394a3924dbc861f2e26690c3b3d19b2
teghfo <teghfo@gmail.com>
2022-11-25 16:19:07 +0330

    W10: custom provider

--- hello_graphQL/src/todo/todo.module.ts
+++ hello_graphQL/src/todo/todo.module.ts
@@ -1,13 +1,22 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { TodoService } from './todo.service';
-import { TodoResolver } from './todo.resolver';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Todo } from './entities/todo.entity';
-import { UserModule } from '../user/user.module';
+import { forwardRef, Module } from "@nestjs/common";
+import { TodoService } from "./todo.service";
+import { TodoResolver } from "./todo.resolver";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Todo } from "./entities/todo.entity";
+import { UserModule } from "../user/user.module";
+import { PubSub } from "graphql-subscriptions";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo]), forwardRef(() => UserModule)],
-  providers: [TodoResolver, TodoService],
+  providers: [
+    TodoResolver,
+    TodoService,
+    {
+      provide: "PUB_SUB",
+      useValue: new PubSub(),
+      // useFactory: () => new PubSub(),
+    },
+  ],
   exports: [TodoService],
 })
 export class TodoModule {}
--- hello_graphQL/src/todo/todo.resolver.ts
+++ hello_graphQL/src/todo/todo.resolver.ts
@@ -7,34 +7,36 @@ import {
   Parent,
   ResolveField,
   Subscription,
-} from '@nestjs/graphql';
-import { PubSub } from 'graphql-subscriptions';
-import { TodoService } from './todo.service';
-import { Todo } from './entities/todo.entity';
-import { CreateTodoInput } from './dto/create-todo.input';
-import { UpdateTodoInput } from './dto/update-todo.input';
-import { User } from 'src/user/entities/user.entity';
-
-const pubSub = new PubSub();
+} from "@nestjs/graphql";
+import { PubSubEngine } from "graphql-subscriptions";
+import { TodoService } from "./todo.service";
+import { Todo } from "./entities/todo.entity";
+import { CreateTodoInput } from "./dto/create-todo.input";
+import { UpdateTodoInput } from "./dto/update-todo.input";
+import { User } from "src/user/entities/user.entity";
+import { Inject } from "@nestjs/common";
 
 @Resolver(() => Todo)
 export class TodoResolver {
-  constructor(private readonly todoService: TodoService) {}
+  constructor(
+    private readonly todoService: TodoService,
+    @Inject("PUB_SUB") private pubSub: PubSubEngine
+  ) {}
 
   @Mutation(() => Todo)
-  async createTodo(@Args('createTodoInput') createTodoInput: CreateTodoInput) {
+  async createTodo(@Args("createTodoInput") createTodoInput: CreateTodoInput) {
     const todo = await this.todoService.create(createTodoInput);
-    pubSub.publish('todoAdded', { todoCreated: todo });
+    this.pubSub.publish("todoAdded", { todoCreated: todo });
     return todo;
   }
 
-  @Query(() => [Todo], { name: 'todos' })
+  @Query(() => [Todo], { name: "todos" })
   async findAll() {
     return await this.todoService.findAll();
   }
 
-  @Query(() => Todo, { name: 'todo' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
+  @Query(() => Todo, { name: "todo" })
+  findOne(@Args("id", { type: () => Int }) id: number) {
     return this.todoService.findOne(id);
   }
 
@@ -45,21 +47,21 @@ export class TodoResolver {
 
   @Mutation(() => Todo)
   updateTodo(
-    @Args('id', { type: () => Int }) id: number,
-    @Args('updateTodoInput') updateTodoInput: UpdateTodoInput,
+    @Args("id", { type: () => Int }) id: number,
+    @Args("updateTodoInput") updateTodoInput: UpdateTodoInput
   ) {
     return this.todoService.update(id, updateTodoInput);
   }
 
   @Mutation(() => Todo)
-  async removeTodo(@Args('id', { type: () => Int }) id: number) {
+  async removeTodo(@Args("id", { type: () => Int }) id: number) {
     return await this.todoService.remove(id);
   }
 
   @Subscription(() => Todo, {
-    name: 'todoCreated',
+    name: "todoCreated",
   })
   todoCreatedNotify() {
-    return pubSub.asyncIterator('todoAdded');
+    return this.pubSub.asyncIterator("todoAdded");
   }
 }
